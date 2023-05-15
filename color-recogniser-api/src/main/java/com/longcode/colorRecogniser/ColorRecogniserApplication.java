431744650112095f1984f8f1b09d23145f8e11c3
Duy Long Nguyen <longlunglay1998@gmail.com>
2023-05-16 00:43:28 +1000

    Change framework

---
+++ color-recogniser-api/src/main/java/com/longcode/colorRecogniser/ColorRecogniserApplication.java
@@ -0,0 +1,13 @@
+package com.longcode.colorRecogniser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorRecogniserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorRecogniserApplication.class, args);
+	}
+
+}
---
+++ color-recogniser-api/src/test/java/com/longcode/colorRecogniser/ColorRecogniserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longcode.colorRecogniser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorRecogniserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
