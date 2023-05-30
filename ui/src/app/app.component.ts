0f956a8a96ba010a98310de2aa26b6610cec3980
Duy Long Nguyen <longlunglay1998@gmail.com>
2023-05-31 00:35:43 +1000

    SEO

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { PrimeNGConfig } from "primeng/api";
 import { ThemeService } from "./shared/services/theme.service";
-import { Title } from "@angular/platform-browser";
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
@@ -14,9 +14,17 @@ export class AppComponent {
   constructor(
     private $primengConfig: PrimeNGConfig,
     private $theme: ThemeService,
-    private $title: Title
+    private $title: Title,
+    private $meta: Meta
   ) {
     this.$primengConfig.ripple = true;
     $title.setTitle("Color Recogniser");
+    $meta.addTags([
+      {
+        name: "description",
+        content:
+          "Discover a world of vibrant colors with Color Recogniser. Our easy-to-use tool helps you recognize and explore colors in photos, making it perfect for kids, art teachers, artists, photographers, and designers. Unleash your creativity and unlock the palette of possibilities with our accurate color recognition tool.",
+      },
+    ]);
   }
 }
