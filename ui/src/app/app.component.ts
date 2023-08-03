9ee890f83185aac3c5db4968acdf13f3f19c9184
Duy Long Nguyen <longlunglay1998@gmail.com>
2023-08-04 09:29:55 +1000

    Added cookie accepted date

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { ConfirmationService, PrimeNGConfig } from "primeng/api";
 import { Meta, Title } from "@angular/platform-browser";
 import { RouteEnum } from "./shared/utils";
+import { LocalforageService } from "./shared/services/localforage.service";
+import { map } from "rxjs";
+
+const CookieConsentAcceptedDate = "CookieConsentAcceptedDate";
 
 @Component({
   selector: "app-root",
@@ -11,14 +15,13 @@ import { RouteEnum } from "./shared/utils";
 })
 export class AppComponent implements OnInit {
   title = "color-recogniser";
-  cookieDialogVisible = true;
+  cookieDialogVisible = false;
   privacyPolicyUrl = RouteEnum.PrivacyPolicy;
 
   constructor(
     private $primengConfig: PrimeNGConfig,
     $title: Title,
-    private $meta: Meta,
-    private $confirmation: ConfirmationService
+    private $localForage: LocalforageService
   ) {
     this.$primengConfig.ripple = true;
     $title.setTitle("Color Recognizer");
@@ -31,15 +34,27 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.$confirmation.confirm({
-      message:
-        "This website uses cookies to enhance your experience. By continuing to use this site, you consent to our use of cookies.",
-      header: "Cookie Consent",
-      icon: "pi pi-info-circle",
-      key: "cookieDialog",
-      rejectVisible: false,
-      closeOnEscape: true,
-      dismissableMask: true,
-    });
+    this.$localForage
+      .getItem<Date>(CookieConsentAcceptedDate)
+      .pipe(
+        map((date) => {
+          if (!date) {
+            this.cookieDialogVisible = true;
+          } else {
+            date.setDate(date.getDate() + 1);
+            if (date.getTime() < new Date().getTime()) {
+              this.cookieDialogVisible = true;
+            }
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  acceptCookies() {
+    this.cookieDialogVisible = false;
+    this.$localForage
+      .setItem(CookieConsentAcceptedDate, new Date())
+      .subscribe();
   }
 }
