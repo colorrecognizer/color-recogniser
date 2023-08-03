6f09deee4aa656b163680869b0a83b3344f02480
Duy Long Nguyen <longlunglay1998@gmail.com>
2023-08-03 11:57:14 +1000

    Update

--- ui/src/app/app-pages/privacy-policy/privacy-policy.component.ts
+++ ui/src/app/app-pages/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+import { RouteEnum } from "src/app/shared/utils";
 
 @Component({
   selector: "app-privacy-policy",
   templateUrl: "./privacy-policy.component.html",
-  styleUrls: ["./privacy-policy.component.scss"]
+  styleUrls: ["./privacy-policy.component.scss"],
 })
 export class PrivacyPolicyComponent {
-
+  contactUsUrl = RouteEnum.ContactUs;
 }
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from "@angular/core";
-import { PrimeNGConfig } from "primeng/api";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { ConfirmationService, PrimeNGConfig } from "primeng/api";
 import { Meta, Title } from "@angular/platform-browser";
+import { RouteEnum } from "./shared/utils";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "color-recogniser";
+  cookieDialogVisible = true;
+  privacyPolicyUrl = RouteEnum.PrivacyPolicy;
 
   constructor(
     private $primengConfig: PrimeNGConfig,
     $title: Title,
-    private $meta: Meta
+    private $meta: Meta,
+    private $confirmation: ConfirmationService
   ) {
     this.$primengConfig.ripple = true;
     $title.setTitle("Color Recognizer");
@@ -23,5 +28,18 @@ export class AppComponent {
     //     content: "https://colorrecognizer.io/assets/images/logo.png",
     //   },
     // ]);
+  }
+
+  ngOnInit(): void {
+    this.$confirmation.confirm({
+      message:
+        "This website uses cookies to enhance your experience. By continuing to use this site, you consent to our use of cookies.",
+      header: "Cookie Consent",
+      icon: "pi pi-info-circle",
+      key: "cookieDialog",
+      rejectVisible: false,
+      closeOnEscape: true,
+      dismissableMask: true,
+    });
   }
 }
