464b8364d95d2c0fa9642773f25dc2376791a42d
Duy Long Nguyen <longlunglay1998@gmail.com>
2023-08-07 09:17:19 +1000

    WIP

--- color-recogniser-api/src/main/java/com/longcode/colorRecogniser/ColorRecogniserApplication.java
+++ color-recogniser-api/src/main/java/com/longcode/colorRecogniser/ColorRecogniserApplication.java
@@ -28,70 +28,70 @@ import java.util.List;
 
 @SpringBootApplication
 public class ColorRecogniserApplication {
-	/**
-	 * Application name.
-	 */
-	private static final String APPLICATION_NAME = "Color Recognizer";
-	/**
-	 * Global instance of the JSON factory.
-	 */
-	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-	/**
-	 * Directory to store authorization tokens for this application.
-	 */
-	private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME = "Color Recognizer";
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-	/**
-	 * Global instance of the scopes required by this quickstart.
-	 * If modifying these scopes, delete your previously saved tokens/ folder.
-	 */
-	private static final List<String> SCOPES = List.of(GmailScopes.GMAIL_COMPOSE,
-			GmailScopes.GMAIL_SEND,
-			GmailScopes.MAIL_GOOGLE_COM,
-			GmailScopes.GMAIL_MODIFY);
-	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = List.of(GmailScopes.GMAIL_COMPOSE,
+            GmailScopes.GMAIL_SEND,
+            GmailScopes.MAIL_GOOGLE_COM,
+            GmailScopes.GMAIL_MODIFY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-	public static Gmail gmailService;
+    public static Gmail gmailService;
 
-	/**
-	 * Creates an authorized Credential object.
-	 *
-	 * @param HTTP_TRANSPORT The network HTTP Transport.
-	 * @return An authorized Credential object.
-	 * @throws IOException If the credentials.json file cannot be found.
-	 */
-	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-			throws IOException {
-		// Load client secrets.
-		InputStream in = ColorRecogniserApplication.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-		if (in == null) {
-			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-		}
-		GoogleClientSecrets clientSecrets =
-				GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException {
+        // Load client secrets.
+        InputStream in = ColorRecogniserApplication.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-				HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-				.setAccessType("offline")
-				.build();
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-		//returns an authorized Credential object.
-		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-	}
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        //returns an authorized Credential object.
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 
-	public static void gmailStart() throws GeneralSecurityException, IOException {
-		// Build a new authorized API client service.
-		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		gmailService = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME)
-				.build();
-	}
+    public static void gmailStart() throws GeneralSecurityException, IOException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        gmailService = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
-	public static void main(String[] args) throws GeneralSecurityException, IOException {
-//		gmailStart();
-			SpringApplication.run(ColorRecogniserApplication.class, args);
-	}
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        gmailStart();
+        SpringApplication.run(ColorRecogniserApplication.class, args);
+    }
 
 }
