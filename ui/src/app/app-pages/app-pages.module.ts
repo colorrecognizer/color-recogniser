8ca209f505b2115c4c75d2d9e826384a35368824
toannd1504 <toanyeuchuyenhalong123@gmail.com>
2023-10-28 07:26:52 +0700

    create root table in generate-md-table tab

--- ui/src/app/app-pages/app-pages.module.ts
+++ ui/src/app/app-pages/app-pages.module.ts
@@ -6,7 +6,7 @@ import { AppPagesComponent } from "./app-pages.component";
 import { NavbarModule } from "./navbar/navbar.module";
 import { FooterComponent } from "./footer/footer.component";
 import { RegularlyUsedPrimengModule } from "../shared/modules/regularly-used-primeng.module";
-
+import { TableModule } from "primeng/table";
 @NgModule({
   declarations: [AppPagesComponent, FooterComponent],
   imports: [
@@ -14,6 +14,7 @@ import { RegularlyUsedPrimengModule } from "../shared/modules/regularly-used-pri
     AppPagesRoutingModule,
     NavbarModule,
     RegularlyUsedPrimengModule,
+    TableModule,
   ],
 })
 export class AppPagesModule {}
