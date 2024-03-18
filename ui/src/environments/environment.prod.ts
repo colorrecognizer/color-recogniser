d9a62e991c39ad9a083621aa401e9535dd3dd812
Duy Long Nguyen <longlunglay1998@gmail.com>
2024-03-18 22:52:46 +1100

    Remove unnecessary code and update API URL

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: "https://www.colorrecogniser.com",
-  firebase: {
-    apiKey: "AIzaSyCUuPeiEBlTpFtOmx4pYQ5WXJhZvdq4I5M",
-    authDomain: "color-recogniser-b18e3.firebaseapp.com",
-    projectId: "color-recogniser-b18e3",
-    storageBucket: "color-recogniser-b18e3.appspot.com",
-    messagingSenderId: "926203459913",
-    appId: "1:926203459913:web:214157a3e707c15f010298",
-    measurementId: "G-F4QMQ9L4L2",
-  },
+  apiUrl: "https://www.colorrecogniser.com"
 };
 
 /*
